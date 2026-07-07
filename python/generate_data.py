d824c5d1e5fd92f2c0965598d96c53ddf2e27746
Piyush Shahu <piyushshahu2004@gmail.com>
2026-07-07 08:34:48 +0530

    Generate product dataset using Python

--- python/generate_data.py
+++ python/generate_data.py
@@ -1,14 +1,28 @@
+# python .\python\generate_data.py
+# # import pandas as pd
+
+# # print("Welcome to NovaMart Retail Analytics!")
+
+# # data = {
+# #     "Product": ["Laptop", "Mouse", "Keyboard"],
+# #     "Price": [65000, 800, 2500],
+# #     "Stock": [25, 150, 75]
+# # }
+
+# # df = pd.DataFrame(data)
+
+# # print("\nInventory Data:")
+# # print(df)
 import pandas as pd
-
 print("Welcome to NovaMart Retail Analytics!")
-
-data = {
-    "Product": ["Laptop", "Mouse", "Keyboard"],
-    "Price": [65000, 800, 2500],
-    "Stock": [25, 150, 75]
+products = {
+    "product_id": [],
+    "product_name": []
 }
 
-df = pd.DataFrame(data)
-
+for i in range(1,101):
+    products["product_id"].append(i)
+    products["product_name"].append(f"Product {i}")
+pr = pd.DataFrame(products)
 print("\nInventory Data:")
-print(df)
+print(pr)
