6fc919ad4ac3bd3f38799adcbd0854bc52fea180
Piyush Shahu <piyushshahu2004@gmail.com>
2026-07-08 07:56:24 +0530

    Added category and brand generation

--- python/generate_data.py
+++ python/generate_data.py
@@ -15,14 +15,31 @@
 # # print(df)
 import pandas as pd
 print("Welcome to NovaMart Retail Analytics!")
+categories = [
+    "Electronics",
+    "Furniture",
+    "Grocery",
+    "Clothing",
+    "Sports"
+]
+category_brands = {
+    "Electronics": ["Asus", "HP", "Dell", "Samsung", "Lenevo"],
+    "Furniture": ["Godrej", "Nilkamal", "IKEA", "Damro", "Pepperfry"],
+    "Grocery": ["TATA", "Amul", "Dinshaw", "Parle", "Fortune"],
+    "Clothing": ["Gucchi", "Lacoste", "Levis", "Balenciaga", "Raymond"],
+    "Sports": ["Puma", "Adidas", "Nike", "HRX", "Decathalon"]
+}
 products = {
     "product_id": [],
-    "product_name": []
+    "product_name": [],
+    "category": [],
+    "brand": []
 }
-
 for i in range(1,101):
     products["product_id"].append(i)
     products["product_name"].append(f"Product {i}")
-pr = pd.DataFrame(products)
+    products["category"].append(categories[(i-1) % len(categories)])
+    products["brand"].append(category_brands[categories[(i-1) % len(categories)]][(i-1) % len(categories)]) # "(i-1) % len(categories)" instead of this we can write 0  also for selecting first brand name fromm each category
+products_df = pd.DataFrame(products)                        
 print("\nInventory Data:")
-print(pr)
+print(products_df)
